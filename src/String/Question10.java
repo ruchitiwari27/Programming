e6e77e28d48a5680576fc1c6e3a0d964f8209acb
Ruchi <ruchi27tiwari@gmail.com>
2024-06-07 16:02:10 +0530

    letteronly

--- src/String/Question10.java
+++ src/String/Question10.java
@@ -1,2 +1,17 @@
-package String;public class Question10 {
+package String;
+
+public class Question10 {
+    public static void main(String[]args){
+        String s="ksjdhujhf";
+
+        boolean containsOnlyLetters = true;
+        for (char ch : s.toCharArray()) {
+            if (!Character.isLetter(ch)) {
+                containsOnlyLetters = false;
+                break;
+            }
+        }
+        System.out.println("Contains only Letters: " + containsOnlyLetters);
+    }
+
 }
