5700871ea0708b7560ebdfe9b274330b14f88ee2
Ruchi <ruchi27tiwari@gmail.com>
2024-06-07 16:14:52 +0530

    Java Program to Find Duplicate Words in a String

--- src/String/Question3.java
+++ src/String/Question3.java
@@ -1,2 +1,24 @@
-package String;public class Question3 {
+package String;
+
+public class Question3 {
+    public static void main(String[] args) {
+        String s = "THIS IS is  the THE program of find duplicate word in program";
+        s = s.toLowerCase();
+        String[] words = s.split(" ");
+        int count = 0;
+        System.out.println("Duplicate words in the given string:");
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                    // Set words[j] to "0" to avoid printing visited word
+                    words[j] = "0";
+                }
+            }
+            if (count > 1 && !words[i].equals("0")) {
+                System.out.println(words[i]);
+            }
+        }
+    }
 }
