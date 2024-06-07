9dd1cb038e326b05e99d080300f65f0a72e0c53b
Ruchi <ruchi27tiwari@gmail.com>
2024-06-07 16:13:08 +0530

    Java Program to Find Duplicate Characters in a String

--- src/String/Question2.java
+++ src/String/Question2.java
@@ -1,2 +1,23 @@
-package String;public class Question2 {
+
+package String;
+
+public class Question2 {
+    public static void main(String[] args) {
+        String s = "this is  ruchiiiiii";
+        char[] ch = s.toCharArray();
+        int count = 0;
+        System.out.println(s);
+
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = i + 1; j < ch.length; j++) {
+                if (ch[i] == ch[j] && ch[i] != '0') { // Check duplicates and ignore already counted characters
+                    System.out.println("Duplicate character: " + ch[j]);
+                    count++;
+                    ch[j] = '0'; // Mark the character as counted
+                }
+            }
+        }
+
+        System.out.println("the string "+s+" Count of duplicate characters: " + count);
+    }
 }
